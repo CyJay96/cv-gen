7ab784668aae52370ae7a6be746a05e4e3b91564
CyJay96 <konstantin201586@gmail.com>
2024-03-27 13:50:36 +0300

    added sider component

--- src/app/modules/shared/shared.module.ts
+++ src/app/modules/shared/shared.module.ts
@@ -4,12 +4,14 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { SharedRoutingModule } from './shared-routing.module';
 import { SharedComponent } from './shared.component';
+
 import { HeaderComponent } from './components/header/header.component';
+import { SiderComponent } from './components/sider/sider.component';
 
 @NgModule({
-  declarations: [SharedComponent, HeaderComponent],
+  declarations: [SharedComponent, HeaderComponent, SiderComponent],
   imports: [CommonModule, SharedRoutingModule, HttpClientModule],
-  exports: [HeaderComponent],
+  exports: [HeaderComponent, SiderComponent],
   providers: [],
   bootstrap: [SharedComponent],
 })
